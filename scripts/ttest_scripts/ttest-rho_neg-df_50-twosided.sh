9af893b6ab86f773efcc0990fe563b4e8b52372e
Junu Lee <junulee@Junus-MBP-2.lan>
2024-04-30 18:29:53 -0400

    mistake in the scripts

--- scripts/ttest_scripts/ttest-rho_neg-df_50-twosided.sh
+++ scripts/ttest_scripts/ttest-rho_neg-df_50-twosided.sh
@@ -25,6 +25,5 @@ startseed=$(( $whichseed * $n_seeds_per ))
 temp=$(( $startseed + $n_seeds_per )) 
 endseed=$(( ($temp < $n_exp ? $temp : $n_exp) - 1))   # make sure we do exactly n_exp experiments
 
-python -u ttesting_CC.py --name '04302024-ttest-rho_neg-df_50-twosided' --start_seed $startseed --end_seed $endseed --m 100 --df 50 --tp 10 --tp_first --amp $amp --alpha_fdr 0.05 --rho -0.9 --true_sigma_sq 1.1 --rel_mc_error 0.1 --mc_max_samples 5000 --mc_batch_size 100 --exact_cs_switchpoint 0.6 --confidence_required --power_guarantee --e_method 'lrt' --e_alt 'exact' --mask 'pvalues'
-
+python -u ttesting_CC.py --name '04302024-ttest-rho_neg-df_50-twosided' --start_seed $startseed --end_seed $endseed --m 100 --df 50 --tp 10 --tp_first --amp $amp --alpha_fdr 0.05 --rho -0.9 --true_sigma_sq 1.1 --rel_mc_error 0.1 --mc_max_samples 5000 --mc_batch_size 100 --exact_cs_switchpoint 0.6 --confidence_required --power_guarantee --e_method 'lrt' --e_alt 'exact' --mask 'pvalues'  --tp_random_sign 
 
