5ffd6ff9ecdbb39cde656f18eee623e88814fe91
Junu Lee <junulee@Junus-MBP-2.lan>
2024-06-18 13:21:35 -0400

    making less correlation for ztest negative rho exp

--- scripts/ztest_scripts/ztest-alt_3-rho_neg.sh
+++ scripts/ztest_scripts/ztest-alt_3-rho_neg.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#$ -N ztest-alt_3-rho_neg
+#$ -N ztest-alt_3-rho_neg_0.5
 #$ -t 2-12
 #$ -o job_output/$JOB_NAME/$JOB_ID-$TASK_ID.log
 #$ -j y
@@ -11,4 +11,4 @@ amp=$(bc <<<"scale=3;$SGE_TASK_ID/2") # 1, 1.5, ..., 5.5, 6
 
 alt=3
 
-python -u ztesting_CC.py --name 'ztest-alt_3-rho_neg' --n_exp 1000 --m 100 --tp 10 --tp_first --amp $amp --alpha_fdr 0.05 --rho -0.9 --e_method 'lrt' --e_alt $alt --power_guarantee --rel_mc_error 0.1  --mc_batch_size 100 --mc_max_samples 5000 --exact_cs_switchpoint 0.6 --confidence_required --mask 'pvalues' 
+python -u ztesting_CC.py --name 'ztest-alt_3-rho_neg_0.5' --n_exp 1000 --m 100 --tp 10 --tp_first --amp $amp --alpha_fdr 0.05 --rho -0.5 --e_method 'lrt' --e_alt $alt --power_guarantee --rel_mc_error 0.1  --mc_batch_size 100 --mc_max_samples 5000 --exact_cs_switchpoint 0.6 --confidence_required --mask 'pvalues' 
